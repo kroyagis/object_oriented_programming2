8472f596c3825fe24829ccd243c857f1fa76ba9a
Ji You <kroyagis@gmail.com>
2016-06-06 01:21:06 -0400

    Completed

--- sales_taxes/sales_taxes.rb
+++ sales_taxes/sales_taxes.rb
@@ -1,29 +1,17 @@
 class Item
-  attr_accessor :name, :quantity, :unit_price, :tax_category
 
-  def initialize(name, quantity, price, category)
+  attr_accessor :name, :quantity, :unit_price, :unit_total, :unit_tax
+
+  def initialize(name, quantity, unit_price, unit_tax = 1.1, unit_total = 0)
     @name = name
     @quantity = quantity
-    @price = price
-    @category = category
-  end
-
-  def which_category(item)
-
-  end
-end
-
-class Receipt
-
-  def print_receipt(shopping_cart)
-
-  end
-
-  def calculate_price
-
+    @unit_price = unit_price
+    @unit_tax = unit_tax
+    @unit_total = unit_total
   end
 
 end
+
 
 # exempt: books, food, medical products
 #
--- sales_taxes/shopping_cart.rb
+++ sales_taxes/shopping_cart.rb
@@ -1,15 +1,64 @@
-class ShoppingCart
+require_relative 'sales_taxes'
+
+class Receipt < Item
+
+  EXEMPT_ITEM = ['book', 'pill', 'chocolate']
+  attr_accessor :sales_taxes, :total
+
+  def calculate_total
+    @sales_taxes = 0
+    @total = 0
+    @shopping_cart.each do |item|
+      EXEMPT_ITEM.each do |exempt|
+        item.name.each do |name|
+          if name.include?(exempt)
+           item.unit_tax -= 0.1
+          end
+        end
+      end
+      if item.name.include?('imported')
+        item.unit_tax += 0.05
+      end
+      item.unit_total = (item.quantity * item.unit_price) * item.unit_tax
+      @sales_taxes += item.unit_price * (item.unit_tax - 1)
+      @total += item.unit_total
+      puts "#{item.quantity} #{item.name}: #{item.unit_total}"
+    end
+    puts "Sales Taxes: #{@sales_taxes}"
+    puts "Total: #{@total}"
+    return @total
+  end
+end
+
+class ShoppingCart < Receipt
+
+  attr_accessor :shopping_cart
 
   def initialize
-    @shopping_carts = []
+    @shopping_cart = []
   end
 
-  def add_item(item)
-    if item.is_a?(Item)
-      @shopping_carts << item
-    else
-      raise "You must add an item"
+  def start_shopping
+    i = 0
+    counter = 0
+    while i == 0
+      counter += 1
+      user_input = gets.chomp
+      if user_input == 'quit'
+        return @shopping_cart
+        i += 1
+      else
+        @shopping_cart << Item.new(clean_input(user_input)[1..(clean_input(user_input).length)-2], clean_input(user_input)[0].to_i, clean_input(user_input).last.to_f)
+      end
     end
+    return @shopping_cart
   end
-  
+
+  def clean_input(user_input)
+    new_user_input = []
+    user_input = user_input.gsub(/\sat\s/, " ")
+    new_user_input = user_input.split(" ")
+    return new_user_input
+  end
+
 end
